ad3d6c2404df0b974da9c42a1b6386a96214f305
Tobyyip <yuhinyiptoby@gmail.com>
2026-03-11 15:57:31 +0800

    fix: align Java version to 21 for CI build & Modified properties and test case

--- src/test/java/com/toby/todoapi/ToDoListApplicationTests.java
+++ src/test/java/com/toby/todoapi/ToDoListApplicationTests.java
@@ -2,8 +2,10 @@ package com.toby.todoapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ToDoListApplicationTests {
 
     @Test
